67570897195a6a8f893c980c3f40554c1abdaa3f
andela-uibrahim <usman.ibrahim@andela.com>
2017-03-14 13:47:13 +0100

    refactored codes to meet proper style guilds requirement

--- 06-building-a-program-start-to-finish/solving-the-problem/output/helper.js
+++ 06-building-a-program-start-to-finish/solving-the-problem/output/helper.js
@@ -2,37 +2,36 @@
 * helper object for InvertedIndex class
 * @object
 */
-const helpers =
-  {
+const helpers = {
   /** validates file input types
    *
    * @param  {object} file - file to validate
    * @return {boolean} - true or false
    */
-    fileIsValid(file) {
-      if (!file.name.toLowerCase().match(/\.json$/)) {
-        return false;
-      }
-      return true;
-    },
+  fileIsValid(file) {
+    if (!file.name.toLowerCase().match(/\.json$/)) {
+      return false;
+    }
+    return true;
+  },
 
   /** validates content of uploaded file
    *
    * @param  {array} file - file to validate content
    * @return {boolean} result true or false
    */
-    validFileContent: (file) => {
-      let result = true;
-      if (!(file instanceof Array)) {
-        return false;
+  validFileContent: (file) => {
+    let result = true;
+    if (!(file instanceof Array)) {
+      return false;
+    }
+    file.forEach((doc) => {
+      if ((!doc.title) || (!doc.text)) {
+        result = false;
       }
-      file.forEach((doc) => {
-        if ((!doc.title) || (!doc.text)) {
-          result = false;
-        }
-      });
-      return result;
-    },
+    });
+    return result;
+  },
 
   /** removes duplicates from sorted file documents
    * and extract unique words token
@@ -40,35 +39,35 @@ const helpers =
    * @param  {Array} filteredContent - Array of filtered book documents
    * @return {Array} tokens - Array of unique words.
    */
-    getToken: (filteredContent) => {
-      const freshArray = helpers.mergeDocuments(filteredContent);
-      const tokens = helpers.removeDuplicates(freshArray);
-      return tokens;
-    },
+  getToken: (filteredContent) => {
+    const freshArray = helpers.mergeDocuments(filteredContent);
+    const tokens = helpers.removeDuplicates(freshArray);
+    return tokens;
+  },
 
   /** remove repeated words from each documents in file
    *
    * @param  {Array} filteredDocs - Array of filtered documents
    * @return {boolean} - true or false
    */
-    removeDocDuplicates: (filteredDocs) => {
-      const filteredContents = [];
-      filteredDocs.forEach((doc) => {
-        filteredContents.push(helpers.removeDuplicates(doc));
-      });
-      return filteredContents;
-    },
+  removeDocDuplicates: (filteredDocs) => {
+    const filteredContents = [];
+    filteredDocs.forEach((doc) => {
+      filteredContents.push(helpers.removeDuplicates(doc));
+    });
+    return filteredContents;
+  },
 
   /** removes repeated words from an array
    *
    * @param  {object} doc - document to remove duplicates from
    * @return {array} newlist - array of filtered contents
    */
-    removeDuplicates: (doc) => {
-      const newList = doc.filter((word, index) =>
+  removeDuplicates: (doc) => {
+    const newList = doc.filter((word, index) =>
       doc.indexOf(word) === index);
-      return newList;
-    },
+    return newList;
+  },
 
   /** applies the filterContent function on all
    * the documents in the selectedBook
@@ -76,15 +75,15 @@ const helpers =
    * @param  {Array} file - an array of books with title and text
    * @return {Array} filteredBook - Array of filtered book contents
    */
-    filterFileContent: (file) => {
-      const filteredDocs = [];
-      file.forEach((content) => {
-        filteredDocs.push(helpers.filterContent(content.title,
+  filterFileContent: (file) => {
+    const filteredDocs = [];
+    file.forEach((content) => {
+      filteredDocs.push(helpers.filterContent(content.title,
         content.text));
-      });
-      const filteredBook = helpers.removeDocDuplicates(filteredDocs);
-      return filteredBook;
-    },
+    });
+    const filteredBook = helpers.removeDocDuplicates(filteredDocs);
+    return filteredBook;
+  },
 
   /** filters and return an array of filtered
    *  string with special characters removed
@@ -93,15 +92,15 @@ const helpers =
    * @param  {String} text - text in book
    * @return {Array} words - Array of filtered book content
    */
-    filterContent: (title, text) => {
-      text = text || '';
-      let words = (`${title} ${text}`)
+  filterContent: (title, text) => {
+    text = text || '';
+    let words = (`${title} ${text}`)
       .replace(/[^a-zA-Z ]/g, '')
       .toLowerCase()
       .split(' ');
-      words = words.filter(str => /\S/.test(str));
-      return words;
-    },
+    words = words.filter(str => /\S/.test(str));
+    return words;
+  },
 
   /** combines all the documents in a book
    * and returns an Array of sorted strings
@@ -110,13 +109,13 @@ const helpers =
    * @return {Array} sortedArrays - Array of comebined
    * and sorted filtered book content
    */
-    mergeDocuments: (filteredContents) => {
-      let allStr = '';
-      filteredContents.forEach((z) => {
-        allStr += `${z.join(' ')} `;
-      });
-      const sortedArrays = allStr.trim().split(' ').sort();
-      return sortedArrays;
-    },
+  mergeDocuments: (filteredContents) => {
+    let allStr = '';
+    filteredContents.forEach((z) => {
+      allStr += `${z.join(' ')} `;
+    });
+    const sortedArrays = allStr.trim().split(' ').sort();
+    return sortedArrays;
+  },
 
-  };
+};
