c1a97751e3be0c709c142180b3d212a793e43319
Anthony Ngene <anthony.ngene@andela.com>
2017-03-30 18:38:39 +0100

    updated the InvertedIndex class and tests based on feedback

--- 06-building-a-program-start-to-finish/solving-the-problem/output/InvertedIndex.js
+++ 06-building-a-program-start-to-finish/solving-the-problem/output/InvertedIndex.js
@@ -9,36 +9,34 @@ class InvertedIndex {
    */
   constructor() {
     this.files = [];
-    this.showAllFiles = false;
-    this.indexed = {};
-    this.showAllFilesSearch = false;
-    this.searchAllResult = {};
   }
 
   /**
    * Validates file content
    * @function
    * @param {Array} fileContent content of uploaded file
-   * @return {Array} valid fileContent or false if invalid file
+   * @return {Array|Boolean} valid fileContent or false if invalid file
    */
   static validateFile(fileContent) {
-    if (fileContent.constructor !== Array || fileContent.length < 1) {
+    if (!(fileContent instanceof Array) || fileContent.length < 1) {
       return false;
     }
+
     let invalid = false;
     fileContent.forEach((book) => {
-      if (book.title === undefined || book.text === undefined) {
+      if (!(book.title && book.text)) {
         invalid = true;
         return;
       }
       if (book.text.length < 1) invalid = true;
     });
+
     if (invalid) return false;
     return fileContent;
   }
 
   /**
-   * Remove invalid characters
+   * Strips out special characters and removes duplicate words
    * @function
    * @param {String} text text from the book
    * @return {Array} An array of unique words in the text
@@ -58,94 +56,76 @@ class InvertedIndex {
    */
   createIndex(fileContent, filename) {
     if (this.files.indexOf(filename) !== -1) return false;
+
     const result = { bookTitles: [], words: {}, allWords: [], filename };
-    const fileLength = fileContent.length;
-    fileContent.forEach((book, bookNumber) => {
+    fileContent.forEach((book, bookIndex) => {
       result.bookTitles.push(book.title);
       const words = InvertedIndex.tokenize(book.text);
       words.forEach((word) => {
-        if (result.words[word] === undefined) {
+        if (!result.words[word]) {
           result.allWords.push(word);
-          result.words[word] = new Array(fileLength).fill(false);
+          result.words[word] = new Array(fileContent.length).fill(false);
         }
-        result.words[word][bookNumber] = true;
+        result.words[word][bookIndex] = true;
       });
     });
-    this.showAllFiles = false;
+
     this.files.push(filename);
-    this.indexed = result;
     this[filename] = result;
     return true;
   }
 
   /**
-   * Returns stored index for a file given a filename
+   * Returns stored index for a file given the filename
    * @function
    * @param {Object} filename name of the file
-   * @return {Array} indexed object or false if file was not found
+   * @return {Array|Boolean} indexed object or false if file was not found
    */
   getIndex(filename) {
-    if (this[filename] === undefined) return false;
-    this.showAllFiles = false;
-    this.indexed = this[filename];
-    return this.indexed;
+    if (!this[filename]) return false;
+    return this[filename];
   }
 
   /**
-   * Search index
+   * Searches the index
    * @function
    * @param {String} searchKey string containing word(s) to be searched for
-   * @param {Object} filename name of indexed file where
+   * @param {Object} filename (optional) name of an indexed file where
    * the search will be performed
-   * @return {Object} search result or false if nothing was found
+   * @return {Object|Boolean} search result or false if nothing was found
    */
-  searchIndex(searchKey, filename) {
-    if (searchKey === undefined || searchKey.length < 1
-      || this[filename] === undefined) {
-      return false;
-    }
+  searchIndex(searchKey, filename = false) {
+    if (typeof searchKey !== 'string' || searchKey === '') return false;
     const searchTerms = InvertedIndex.tokenize(searchKey);
-    const result = { bookTitles: [], words: {}, allWords: [] };
-    searchTerms.forEach((word) => {
-      if (this[filename].words[word] !== undefined) {
-        result.allWords.push(word);
-        result.words[word] = this[filename].words[word];
-      }
-    });
-    if (result.allWords.length < 1) return false;
-    result.bookTitles = this[filename].bookTitles;
-    return result;
-  }
-
-  /**
-   * Search All
-   * @function
-   * @param {String} searchKey string containing word(s) to be searched for
-   * @return {Object} search result or false if nothing was found
-   */
-  searchAll(searchKey) {
     const searchAllResult = { files: [] };
-    this.files.forEach((filename) => {
-      const result = this.searchIndex(searchKey, filename);
-      if (result) {
-        searchAllResult.files.push(filename);
-        searchAllResult[filename] = result;
-      }
+
+    const collection = (filename) ? [filename] : this.files;
+    collection.forEach((file) => {
+      const result = { words: {}, allWords: [] };
+      searchTerms.forEach((word) => {
+        if (this[file].words[word]) {
+          result.allWords.push(word);
+          result.words[word] = this[file].words[word];
+        }
+      });
+      if (result.allWords.length < 1) return;
+      searchAllResult.files.push(file);
+      searchAllResult[file] = result;
     });
+
     if (searchAllResult.files.length < 1) return false;
     return searchAllResult;
   }
 
   /**
-   * Delete file from the index
+   * Deletes the index of a file
    * @function
    * @param {String} filename name of file to be deleted
    * @return {boolean} true if file was successfully deleted, else false
    */
-  deleteFile(filename) {
-    if (this[filename] === undefined) return false;
-    this.showAllFilesSearch = false;
-    this.showAllFiles = true;
+  deleteFileIndex(filename) {
+    if (!this[filename]) return false;
+
     this[filename] = undefined;
     this.files.splice(this.files.indexOf(filename), 1);
     return true;
--- 06-building-a-program-start-to-finish/writing-tests/output/invertedIndexSpec.js
+++ 06-building-a-program-start-to-finish/writing-tests/output/invertedIndexSpec.js
@@ -1,23 +1,21 @@
-const validFile = require('./books/validFile.json');
-const simpleValidFile = require('./books/simpleValidFile.json');
+const books = require('./books/validFile.json');
+const shortStories = require('./books/simpleValidFile.json');
 const empty = require('./books/empty.json');
 const emptyText = require('./books/emptyText.json');
 const notArray = require('./books/notArray.json');
 const wrongData = require('./books/wrongData.json');
 
 const index = new InvertedIndex();
+const filename = 'shortStories.json';
 
 describe('InvertedIndex', () => {
   describe('Constructor', () => {
     it('can create an instance of InvertedIndex', () => {
       expect(typeof index).toEqual('object');
     });
+
     it('initializes properties correctly', () => {
       expect(index.files).toEqual([]);
-      expect(index.showAllFiles).toBeFalsy();
-      expect(index.indexed).toEqual({});
-      expect(index.showAllFilesSearch).toBeFalsy();
-      expect(index.searchAllResult).toEqual({});
     });
   });
 
@@ -28,15 +26,17 @@ describe('InvertedIndex', () => {
       expect(InvertedIndex.validateFile(notArray)).toBeFalsy();
       expect(InvertedIndex.validateFile(wrongData)).toBeFalsy();
     });
+
     it('should not return false if a valid file was uploaded', () => {
-      expect(InvertedIndex.validateFile(validFile)).not.toBeFalsy();
-      expect(InvertedIndex.validateFile(simpleValidFile)).not.toBeFalsy();
-      expect(typeof InvertedIndex.validateFile(validFile)).toEqual('object');
+      expect(InvertedIndex.validateFile(books)).not.toBeFalsy();
+      expect(InvertedIndex.validateFile(shortStories)).not.toBeFalsy();
+      expect(typeof InvertedIndex.validateFile(books)).toEqual('object');
     });
+
     it('should return the file content if the file is valid', () => {
-      expect(InvertedIndex.validateFile(validFile)).toEqual(validFile);
-      expect(InvertedIndex.validateFile(simpleValidFile))
-      .toEqual(simpleValidFile);
+      expect(InvertedIndex.validateFile(books)).toEqual(books);
+      expect(InvertedIndex.validateFile(shortStories))
+      .toEqual(shortStories);
     });
   });
 
@@ -45,52 +45,36 @@ describe('InvertedIndex', () => {
       expect(InvertedIndex.tokenize('tony is humble tony'))
       .toEqual(['tony', 'is', 'humble']);
     });
+
     it('should remove invalid characters', () => {
       expect(InvertedIndex.tokenize('$A%^n*del)a %i=s @a$w&es$om#e%'))
       .toEqual(['andela', 'is', 'awesome']);
     });
+
     it('should return lowercase words', () => {
       expect(InvertedIndex.tokenize('HEllO WORLD')).toEqual(['hello', 'world']);
     });
   });
 
   describe('CreateIndex', () => {
-    beforeAll(() => {
-      this.filename = 'simpleValidFile.json';
+    it('creates an index object from the file content', () => {
+      expect(index.createIndex(shortStories, filename)).toBeTruthy();
+      expect(typeof index[filename]).toEqual('object');
     });
 
-    it('creates an index object from the file content', () => {
-      expect(index.createIndex(simpleValidFile, this.filename)).toBeTruthy();
-      expect(typeof index.getIndex(this.filename)).toEqual('object');
-    });
     it('returns false if file already exists', () => {
-      expect(index.createIndex(simpleValidFile, this.filename)).toBeFalsy();
+      expect(index.createIndex(shortStories, filename)).toBeFalsy();
     });
+
     it('correctly stores the total number of books and their titles', () => {
-      expect(index.getIndex(this.filename).bookTitles).toEqual(
+      expect(index[filename].bookTitles).toEqual(
         ['Alice', 'Lord of the Rings', 'Rings']
       );
-      expect(index.getIndex(this.filename).bookTitles.length).toBe(3);
+      expect(index[filename].bookTitles.length).toBe(3);
     });
-    it('creates the correct index object', () => {
-      expect(index.getIndex(this.filename).words.alice)
-      .toEqual([true, false, false]);
-      expect(index.getIndex(this.filename).words.falls)
-      .toEqual([true, true, false]);
-      expect(index.getIndex(this.filename).words.alliance)
-      .toEqual([false, true, true]);
-    });
-    it('correctly stores all words in the file', () => {
-      expect(index.getIndex(this.filename).allWords).toEqual(
-        ['alice', 'falls', 'into', 'an', 'unusual', 'alliance']
-      );
-    });
-  });
 
-  describe('GetIndex', () => {
-    it('should return the correct index', () => {
-      expect(typeof index.getIndex('simpleValidFile.json')).toEqual('object');
-      expect(index.getIndex(this.filename)).toEqual(
+    it('creates the correct index object', () => {
+      expect(index[filename]).toEqual(
         {
           bookTitles: ['Alice', 'Lord of the Rings', 'Rings'],
           allWords: ['alice', 'falls', 'into', 'an', 'unusual', 'alliance'],
@@ -102,82 +86,113 @@ describe('InvertedIndex', () => {
             unusual: [false, true, true],
             alliance: [false, true, true],
           },
-          filename: 'simpleValidFile.json'
+          filename: 'shortStories.json'
         }
       );
     });
+
+    it('correctly stores all words in the file', () => {
+      expect(index[filename].allWords).toEqual(
+        ['alice', 'falls', 'into', 'an', 'unusual', 'alliance']
+      );
+    });
+  });
+
+  describe('GetIndex', () => {
+    it('should return the correct index', () => {
+      expect(typeof index.getIndex('shortStories.json')).toEqual('object');
+      expect(index.getIndex(filename)).toEqual(
+        {
+          bookTitles: ['Alice', 'Lord of the Rings', 'Rings'],
+          allWords: ['alice', 'falls', 'into', 'an', 'unusual', 'alliance'],
+          words: {
+            alice: [true, false, false],
+            falls: [true, true, false],
+            into: [true, false, false],
+            an: [false, true, true],
+            unusual: [false, true, true],
+            alliance: [false, true, true],
+          },
+          filename: 'shortStories.json'
+        }
+      );
+    });
+
     it('should return false if file index was not found', () => {
       expect(index.getIndex('notIndex.json')).toBeFalsy();
     });
-    it('sets the current indexed file to the result', () => {
-      expect(index.getIndex(this.filename)).toEqual(index.indexed);
-    });
   });
 
-  describe('SearchIndex', () => {
-    it('should return false if filename does not exist', () => {
-      expect(index.searchIndex('alice', 'wrongData.json')).toBeFalsy();
-    });
-    it('should return return the correct search result', () => {
-      expect(typeof index.searchIndex('alice', this.filename))
-      .toEqual('object');
-      expect(index.searchIndex('alice hahaha unusual', this.filename)).toEqual(
-        {
-          bookTitles: ['Alice', 'Lord of the Rings', 'Rings'],
-          allWords: ['alice', 'unusual'],
-          words: {
-            alice: [true, false, false],
-            unusual: [false, true, true],
-          }
-        }
-      );
-    });
-    it('should return false if searchKey is an empty string', () => {
-      expect(index.searchIndex('', this.filename)).toBeFalsy();
-    });
-    it('should return false if nothing was found', () => {
-      expect(index.searchAll('abcdefgh#', this.filename)).toBeFalsy();
-    });
-  });
-
-  describe('SeachAll', () => {
+  describe('SeachIndex', () => {
     beforeAll(() => {
-      index.createIndex(validFile, 'validFile.json');
+      index.createIndex(books, 'books.json');
     });
 
     it('should return an object containing the search results', () => {
-      expect(typeof index.searchAll('alice')).toEqual('object');
+      expect(typeof index.searchIndex('alice')).toEqual('object');
     });
+
     it('result should contain an array of files where the search key was found',
     () => {
-      expect(index.searchAll('alice').files).toEqual(
-        ['simpleValidFile.json', 'validFile.json']
+      expect(index.searchIndex('alice').files).toEqual(
+        ['shortStories.json', 'books.json']
         );
     });
+
     it('should return valid search results', () => {
-      expect(index.searchAll('alice')[this.filename]).toEqual(
+      expect(index.searchIndex('alice seek')).toEqual(
         {
-          bookTitles: ['Alice', 'Lord of the Rings', 'Rings'],
-          allWords: ['alice'],
-          words: {
-            alice: [true, false, false],
+          files: ['shortStories.json', 'books.json'],
+          'books.json': {
+            allWords: ['alice', 'seek'],
+            words: {
+              alice: [true, false, false],
+              seek: [false, true, true]
+            }
+          },
+          'shortStories.json': {
+            allWords: ['alice'],
+            words: {
+              alice: [true, false, false]
+            }
           }
         }
       );
     });
+
     it('should return false if nothing was found', () => {
-      expect(index.searchAll('abcdefgh#')).toBeFalsy();
+      expect(index.searchIndex('abcdefgh#')).toBeFalsy();
+    });
+
+    it('should return false if searchKey is an empty string', () => {
+      expect(index.searchIndex('', filename)).toBeFalsy();
+    });
+
+    it('can search correctly when filename is passed in', () => {
+      expect(index.searchIndex('alice hahaha unusual', filename)).toEqual(
+        {
+          files: ['shortStories.json'],
+          'shortStories.json': {
+            allWords: ['alice', 'unusual'],
+            words: {
+              alice: [true, false, false],
+              unusual: [false, true, true],
+            }
+          }
+        }
+      );
     });
   });
 
-  describe('DeleteFile', () => {
+  describe('DeleteFileIndex', () => {
     it('can successfully delete a file from the index', () => {
-      expect(index.deleteFile(this.filename)).toBeTruthy();
-      expect(index.getIndex(this.filename)).toBeFalsy();
-      expect(index.files.indexOf(this.filename)).toBe(-1);
+      expect(index.deleteFileIndex(filename)).toBeTruthy();
+      expect(index.getIndex(filename)).toBeFalsy();
+      expect(index.files.indexOf(filename)).toBe(-1);
     });
+
     it('returns false if filename does not exist in the index', () => {
-      expect(index.deleteFile('abcdef.json')).toBeFalsy();
+      expect(index.deleteFileIndex('abcdef.json')).toBeFalsy();
     });
   });
 });
