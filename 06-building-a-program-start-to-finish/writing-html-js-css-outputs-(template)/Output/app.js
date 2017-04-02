6cbc89db9b069ca95a25a75fe33b0944a3508467
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-04-02 05:44:50 +0100

    validate file against es6

--- 06-building-a-program-start-to-finish/writing-html-js-css-outputs-(template)/Output/app.js
+++ 06-building-a-program-start-to-finish/writing-html-js-css-outputs-(template)/Output/app.js
@@ -1,24 +1,23 @@
-angular.module("InvertedIndexApp", [])
-  .controller("IndexController", ["$scope", ($scope) => {
+angular.module('InvertedIndexApp', [])
+  .controller('IndexController', ['$scope', ($scope) => {
+    const invertedIndex = new InvertedIndex();
+    $scope.fileName = null;
+    $scope.fileNames = [];
+    $scope.indexedFiles = {};
+    $scope.displayIndexes = false;
+    $scope.displaySearchResult = false;
+    $scope.showError = false;
+    $scope.errorHandler = '';
+    $scope.phrase = '';
 
-  const invertedIndex = new InvertedIndex();
-  $scope.fileName = null;
-  $scope.fileNames = [];
-  $scope.indexedFiles = {};
-  $scope.displayIndexes = false;
-  $scope.displaySearchResult = false;
-  $scope.showError = false;
-  $scope.errorHandler = "";
-  $scope.phrase = "";
-
-  $scope.createIndex = (file) => {
-    $scope.fileName = file.name.split('.')[0];
-    if (!file.name.toLowerCase().match(/\.json$/)) {
-      $scope.showError = true;
-      $scope.errorHandler = `${file.name} is not a JSON file.`;
-      return;
-    } else {
-      invertedIndex.readFile(file).then((response) => {
+    $scope.createIndex = (file) => {
+      $scope.fileName = file.name.split('.')[0];
+      if (!file.name.toLowerCase().match(/\.json$/)) {
+        $scope.showError = true;
+        $scope.errorHandler = `${file.name} is not a JSON file.`;
+        return;
+      }
+      InvertedIndex.readFile(file).then((response) => {
         if (response.success) {
           invertedIndex.createIndex($scope.fileName, response.fileToValidate);
           $scope.$apply(() => {
@@ -26,37 +25,34 @@ angular.module("InvertedIndexApp", [])
           });
         } else {
           $scope.errorHandler = `${file.name} ${response.message}`;
-          return;
         }
       });
-    }
-  };
+    };
 
-  $scope.displayIndex = () => {
-    const filename  = $scope.getFile.split('.')[0];
-    $scope.indexedFiles = invertedIndex.getIndex(filename);
-    $scope.displayIndexes = true;
-    $scope.displaySearchResult = false;
-  };
+    $scope.displayIndex = () => {
+      const filename = $scope.getFile.split('.')[0];
+      $scope.indexedFiles = invertedIndex.getIndex(filename);
+      $scope.displayIndexes = true;
+      $scope.displaySearchResult = false;
+    };
 
-  $scope.displaySearch = () => {
-    $scope.searchResults = {};
-    if ($scope.phrase.length < 1) {
-      $scope.errorHandler = "Please enter a word to search";
-    } else {
-      if ($scope.searchOption === "All") {
+    $scope.displaySearch = () => {
+      $scope.searchResults = {};
+      if ($scope.phrase.length < 1) {
+        $scope.errorHandler = 'Please enter a word to search';
+      } else if ($scope.searchOption === 'All') {
         $scope.searchResults = invertedIndex.searchIndex($scope.phrase);
         $scope.displaySearchResult = true;
         $scope.displayIndexes = false;
       } else {
-        const filename  = $scope.searchOption.split('.')[0];
-        $scope.searchResults[filename] = invertedIndex.searchIndex($scope.phrase)[filename];
+        const filename = $scope.searchOption.split('.')[0];
+        $scope.searchResults[filename] =
+          invertedIndex.searchIndex($scope.phrase)[filename];
         $scope.displaySearchResult = true;
         $scope.displayIndexes = false;
-      };
-    }console.log($scope.searchResults);
-  }
-}])
+      }
+    };
+  }])
 .filter('range', () => (input, range) => {
   for (let i = 0; i < parseInt(range, 10); i += 1) {
     input.push(i);
@@ -67,7 +63,7 @@ angular.module("InvertedIndexApp", [])
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('upload')
-    .addEventListener('change', function createFile(file) {
+    .addEventListener('change', function createFile() {
       if (this.files[0]) {
         angular.element(this).scope().createIndex(this.files[0]);
       }
