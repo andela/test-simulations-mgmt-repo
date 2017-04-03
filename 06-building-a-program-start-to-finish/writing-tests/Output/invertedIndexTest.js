b944761554588474f0187284e2f2cc6ed5f8dd32
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-04-03 16:56:02 +0100

    modified tokenize method and camelcase implemented

--- 06-building-a-program-start-to-finish/writing-tests/Output/invertedIndexTest.js
+++ 06-building-a-program-start-to-finish/writing-tests/Output/invertedIndexTest.js
@@ -1,27 +1,28 @@
-/* global InvertedIndexClass */
 import book from './../book.json';
-import bookempty from './../bookempty.json';
-import bookwrongformat from './../bookwrongformat.json';
-import notBook from './../notBook.json';
-import books3 from './../books3.json';
+import emptyBook from './../emptyBook.json';
+import wrongFormat from './../wrongFormat.json';
+import invalidBook from './../invalidBook.json';
+import books from './../books.json';
 
 const myInvertedIndex = new InvertedIndexClass();
-myInvertedIndex.files['book.json'] = book;
-myInvertedIndex.files['books3.json'] = books3;
 
 /* Test Suites */
 describe('Inverted Index Test', () => {
   describe('ReadFile', () => {
+    beforeEach(() => {
+      myInvertedIndex.files['book.json'] = book;
+      myInvertedIndex.files['books.json'] = books;
+    });
     it('should return false when checking an empty JSON array', () => {
-      expect(myInvertedIndex.readFile(bookempty))
+      expect(myInvertedIndex.readFile(emptyBook))
         .toBeFalsy();
     });
-    it('should return false when checking an wrongformatted JSON array', () => {
-      expect(myInvertedIndex.readFile(bookwrongformat))
+    it('should return false when checking a wrongformatted JSON array', () => {
+      expect(myInvertedIndex.readFile(wrongFormat))
         .toBeFalsy();
     });
     it('should return false when book is not an array', () => {
-      expect(myInvertedIndex.readFile(notBook))
+      expect(myInvertedIndex.readFile(invalidBook))
         .toBeFalsy();
     });
     it('should return true if a valid JSON array', () => {
@@ -31,7 +32,7 @@ describe('Inverted Index Test', () => {
 
     it('should return correct keys for files when file is saved', () => {
       expect(Object.keys(myInvertedIndex.files))
-        .toEqual(['book.json', 'books3.json']);
+        .toEqual(['book.json', 'books.json']);
     });
 
     it('should ensure the file content is saved accurrately', () => {
@@ -42,20 +43,24 @@ describe('Inverted Index Test', () => {
   describe('Validate File', () => {
     it('should return false when file does not contain "title" & "text" format'
     , () => {
-      expect(myInvertedIndex.validateFile(bookwrongformat))
+      expect(myInvertedIndex.validateFile(wrongFormat))
       .toBeFalsy();
     });
-  });
-  it('verifies that the JSON file is valid', () => {
-    expect(myInvertedIndex.validateFile(book).length).toEqual(2);
+    it('verifies that the JSON file is valid', () => {
+      expect(myInvertedIndex.validateFile(book).length).toEqual(2);
+    });
   });
   describe('Create Index Table', () => {
+    beforeEach(() => {
+      myInvertedIndex.files['books.json'] = books;
+      myInvertedIndex.files['book.json'] = book;
+    });
     it('should ensure that index is created', () => {
       expect(myInvertedIndex.createIndex('book.json'))
         .toBeTruthy();
     });
     it('should ensure that index is created', () => {
-      expect(myInvertedIndex.createIndex('books3.json'))
+      expect(myInvertedIndex.createIndex('books.json'))
         .toBeTruthy();
     });
   });
@@ -66,23 +71,35 @@ describe('Inverted Index Test', () => {
     });
   });
   describe('Tokenize', () => {
+    beforeEach(() => {
+      myInvertedIndex.files['books.json'] = books;
+    });
     it('should return correct terms in form of string in an array', () => {
-      expect(myInvertedIndex.tokenize(books3[0].text))
+      expect(myInvertedIndex.tokenize(books[0].text))
       .toEqual(['alice', 'falls', 'into', 'a', 'rabbit', 'hole', 'and',
         'enters', 'a', 'world', 'full', 'of', 'imagination']);
     });
+    it('should return correct terms in terms of lowercase', () => {
+      expect(myInvertedIndex.tokenize(books[0].text)[0]).toEqual('alice');
+    });
+    it('should return array of terms without space or invalid characters',
+    () => {
+      expect(myInvertedIndex.tokenize('alice.,&-falls')).toEqual(['alice', 'falls']);
+    });
   });
-
   describe('Search Index', () => {
+    beforeEach(() => {
+      myInvertedIndex.files['books.json'] = books;
+    });
     it('should return correct index of the search term in all books', () => {
       expect(myInvertedIndex.searchIndex('alice'))
-      .toEqual({ 'book.json': { alice: [0] }, 'books3.json': { alice: [0] } });
+      .toEqual({ 'book.json': { alice: [0] }, 'books.json': { alice: [0] } });
     });
     it('should return correct index of the search term in books3.json', () => {
       expect(myInvertedIndex.searchIndex('an alice',
-      ['book.json', 'books3.json']))
+      ['book.json', 'books.json']))
       .toEqual({ 'book.json': { an: [1], alice: [0] },
-        'books3.json': { an: [1, 2], alice: [0] } });
+        'books.json': { an: [1, 2], alice: [0] } });
     });
     it('should return false when no result is found', () => {
       expect(myInvertedIndex.searchIndex('impossibility'))
